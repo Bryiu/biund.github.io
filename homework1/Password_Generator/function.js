069846a9d55fb1697b4c1ac5a3ca630f010cff34
Bryan Iund <bwiund0823@gmail.com>
2019-10-11 12:55:26 -0500

    I have updated the Homework to make it work

--- homework1/Password_Generator/function.js
+++ homework1/Password_Generator/function.js
@@ -1,54 +1,57 @@
 var ysp = document.getElementById("ysp");
 var gp = document.getElementById("gp");
 var cc = document.getElementById("cc");
-tank=[];
+var reset = document.getElementById("reset");
+var password = ("");
+var jumble = ("");
+var tank=[];
 
-gp.addEventListener("click", function makeid(length){
+
+gp.addEventListener("click", function makeid(){
 var charachterLength = parseInt(prompt("How many charachters would you like in your password?"));
-    if ((charachterLength) < 8) {
-        alert("Selection too low");}
-    else if ((charachterLength) > 128) {
-        alert("Selection too high");}   
-    else((charachterLength));   
-        
-    function name(tank){
-confirm("Would you like special charachters?");
-if (confirm = true){
-    var special = ('"',"!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~");
-    console.log(special);
-}
-    else {false};
-}
-    function numeric1(tank){
-confirm("Would you like numeric characters?");
-    if (confirm=true){
-    var numeric =("0","1","2","3","4","5","6","7","8","9");
-       }
-    else{};
-    console.log(numeric);
+    if (charachterLength < 8) {
+        alert("Selection too low");
+    charachterLength = parseInt(prompt("How many charachters would you like in your password?"));
+    }    
+    else if (charachterLength > 128) {
+        alert("Selection too high");
+    charachterLength = parseInt(prompt("How many charachters would you like in your password?"));
+    }   
+    else{
+    } 
+
+    var special = confirm("Would you like special charachters?");
+        if (special === true){
+        var special1 = ['"',"!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+        tank=tank.concat(special1);
     }
-function lowerCase(tank){
-confirm("Would you like lowercase charachters?");
-    if (true){
-    var lowerCase = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+    var numeric = confirm("Would you like numeric characters?");
+        if (numeric===true){
+        var numeric1 =["0","1","2","3","4","5","6","7","8","9"];
+        tank=tank.concat(numeric1);
     }
-    else{};
-console.log(lowerCase);
-} 
-function capital1(tank){
-confirm("Would you like capital charachters?");
-    if (true){
-    var capital =("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    var lowerCase = confirm("Would you like lowercase charachters?");
+        if (lowerCase===true){
+        var lowerCase1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        tank=tank.concat(lowerCase1);
+        }
+    var capital = confirm("Would you like capital charachters?");
+        if (capital===true){
+        var capital1 =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        tank=tank.concat(capital1);
+        }
+    for (i=0; i<charachterLength; i++){
+      var jumble =  Math.floor(Math.random() *tank.length);
+      password = password + tank[jumble];
     }
-    else{};
-    console.log(capital);
-}
+    ysp.innerHTML = password;
 });
 
-
-
- ysp.innerHTML = result
-
+reset.addEventListener("click", function toReset(){
+    tank = [];
+    password = ("");
+    ysp.innerHTML = password;
+});
 
 //to fill an empty array with numbers
 // var arr = new Array();
