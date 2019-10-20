db5878d20c8cbccdb62f1869ede58bc68dcb52f7
Bryan Iund <bwiund0823@gmail.com>
2019-10-19 21:04:42 -0500

    adding Schedule homework

---
+++ homework1/Schedule/readme.txt
@@ -0,0 +1,4 @@
+This is a scheduler that will display different colors depending on the time
+
+When you put in the text and click on the lock it will save the content placed in the 
+text box.
--- homework1/Test_app/function.js
+++ homework1/Test_app/function.js
@@ -1,12 +1,19 @@
-var highscore = document.getElementById("highscore");
+var score = document.getElementById("score");
 var timer = document.getElementById("timer");
-var h2 = document.getElementById("h2");
+var questions = document.getElementById("questions");
 var text = document.getElementById("text");
-var start = document.getElementById("start");
+const start = document.getElementById("start");
 var highscore = document.getElementById("highscore");
 var display = document.getElementById("display");
+let lastQuestionIndex= questions.length-1;
+let runningQuestionIndex=0;
+const time = 10;
+const allTime = 50;
+let count = 0;
+const progress= allTime/time;
+var score1 = 0;
 
-var questions = [
+let question = [
   {
     title: "Why is a moon rock tastier than an earth rock?",
     choices: ["Because its made of cheese", "Rocks are delicious", "This is a terrible joke", "Its a little meteor"],
@@ -31,27 +38,111 @@ var questions = [
     title: "Did you hear about the negative nelly who hates German Sausage?",
     choices: ["Oh boy", "They just wash up on shore", "He always fears the wurst", "10-Tickles"],
     answer: "He always fears the wurst"
-  },
+  }
   
 ];
+//start quiz
+start.addEventListener("click", startQuiz);
+function startQuiz(){
+  start.style.display="none";
+  counter();
+  timer= setInterval(counter, 1000);
+  renderQuestion();
+  container2.style.display = "block";
+}
 
-start.addeventlistener("click", function startQuiz(){
-    setInterval()
+//pop up questions
+function renderQuestion(){
+  let q = questions[runningQuestionIndex];
+  question.innerHTML = "<p>" +q.question+"<p>";
+  choiceA.innerHTML = question.choices[0];
+  choiceB.innerHTML = question.choices[1];
+  choiceC.innerHTML = question.choices[2];
+  choiceD.innerHTML = question.choices[3];
+}
 
+// const time = 10;
+// const allTime = 50;
+// let count = 0;
+// const progress= allTime/time;
+
+
+
+function counter(){
+  if(count <= time){
+      timer.innerHTML = count;
+      count++;
+  }else{
+    count= 0;
+    answerIsWrong();
+    if(runningQuestionIndex < lastQuestionIndex){
+      runningQuestionIndex++;
+      renderQuestion();
+    }else{clearInterval(time)}
+  }
+}
+
+function checkAnswer(answer){
+      if(question[runningQuestionIndex].correct === answer){
+        score++;
+        answerIsCorrect()
+      }else{answerIsWrong()}
+      if(runningQuestionIndex < lastQuestionIndex){
+        count = 0;
+        runningQuestionIndex++;
+        renderQuestion();
+      }else{
+        clearInterval(timer)
+        renderScore();
+      }
+
+}
+
+answerIsWrong(){
+  allTime - 5;
+  runningQuestionIndex++;
+}
+
+
+
+// questions[0].title
+// questions[0].choices[0]
+// questions[0].choices[1]
+// questions[0].choices[2]
+// questions[0].choices[3]
+// questions[0].answer
+
+// start.addeventlistener("click", function startQuiz(){});
+//     h2.innerHTML = questions.choices[i];
+//     for(i=0; i<questions.length; i++){
+
+//     };
+//     document.
 //start the quiz
 //replace text and start quiz timer
 
-answer ++;
+// answer ++;
 
-});
+
+// //18:00
+
 
 //function for looper to display questions
-
-looper(questions[0]);
-looper(questions[1]);
-looper(questions[2]);
-looper(questions[3]);
-looper(questions[4]);
+// for(i=0; i<questions.length; i++){
+//   var response = ;
+//   if (response=== questions[i].answer){
+//     score ++;
+//     alert("correct");
+//     else {
+//       alert("Wrong")
+//     }
+//   }
+// }
+// looper(questions[0]);
+// looper(questions[1]);
+// looper(questions[2]);
+// looper(questions[3]);
+// looper(questions[4]);
 
 
 
